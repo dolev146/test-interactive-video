e6b64562d9d157f21f7c812f08f233f3e57570a3
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2024-06-23 10:14:03 +0300

    work in progress , bug when ending need to create controls our self

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,121 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const videoManager = [
+  {
+    videoName: "video1_badending.mp4",
+    buttons: [
+      {
+        buttonImage: "cart.svg",
+        position: "left",
+        nextVideo: "good_ending1.mp4",
+      },
+      {
+        position: "middle",
+        dreamHeader: "header",
+        dreamBody: "body",
+        dreamFeelings: "feelings",
+      },
+      {
+        buttonImage: "hand.svg",
+        position: "right",
+        nextVideo: "good_ending2.mp4",
+      },
+    ],
+  },
+  {
+    videoName: "good_ending1.mp4",
+    buttons: [
+      { buttonImage: "vite.svg", position: "left", nextVideo: "c.mp4" },
+      {
+        position: "middle",
+        dreamHeader: "header",
+        dreamBody: "body",
+        dreamFeelings: "feelings",
+      },
+      { buttonImage: "vite.svg", position: "right", nextVideo: "c.mp4" },
+    ],
+    prevVideo: "video1_badending.mp4",
+  },
+
+  {
+    videoName: "good_ending2.mp4",
+    buttons: [
+      { buttonImage: "vite.svg", position: "left", nextVideo: "c.mp4" },
+      {
+        position: "middle",
+        dreamHeader: "header",
+        dreamBody: "body",
+        dreamFeelings: "feelings",
+      },
+      { buttonImage: "vite.svg", position: "right", nextVideo: "c.mp4" },
+    ],
+    prevVideo: "video1_badending.mp4",
+  },
+];
+
 function App() {
+  const [currentVideo, setCurrentVideo] = useState(videoManager[0].videoName);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
-    const videoPlayer = document.getElementById("videoPlayer");
-    const buttons = document.querySelectorAll(
-      ".centered-button, .centered-button2, .centered-button3"
-    );
-    console.log(videoPlayer);
+    const videoPlayer = document.getElementById(
+      "videoPlayer"
+    ) as HTMLMediaElement;
 
-    videoPlayer.addEventListener("timeupdate", function () {
-      console.log("timeupdate"), videoPlayer.currentTime;
-      if (videoPlayer.currentTime >= 3 && videoPlayer.currentTime <= 6) {
-        buttons.forEach((button) => button.classList.add("button-visible"));
-      } else {
-        buttons.forEach((button) => button.classList.remove("button-visible"));
-      }
+    videoPlayer.addEventListener("ended", () => {
+      // Show buttons
+      videoManager[currentIndex].buttons.forEach((button, idx) => {
+        const btn = document.querySelector(`.${button.position}-button`);
+        if (btn) btn.classList.add("button-visible");
+      });
     });
+  }, [currentIndex, currentVideo]);
 
-    return () => {};
-  }, []);
+  const changeVideo = (videoName: string) => {
+    const videoIndex = videoManager.findIndex((v) => v.videoName === videoName);
+    if (videoIndex !== -1) {
+      setCurrentVideo(videoManager[videoIndex].videoName);
+      setCurrentIndex(videoIndex);
+      const videoPlayer = document.getElementById(
+        "videoPlayer"
+      ) as HTMLMediaElement;
+      // hide buttons
+      document
+        .querySelectorAll(".left-button, .middle-button, .right-button")
+        .forEach((button) => button.classList.remove("button-visible"));
+      document
+        .querySelectorAll(".left-button, .middle-button, .right-button")
+        .forEach((button) => button.classList.add("opacity0"));
 
-  const toggleVideo = () => {
-    const videoContainer = document.getElementById("videoContainer1");
-    const buttons = document.querySelectorAll(
-      ".centered-button, .centered-button2, .centered-button3"
-    );
-    videoContainer.style.opacity = 0;
-    // Wait for some time before removing the class, 1 second matches your CSS transition time
-    setTimeout(() => {
-      videoContainer.style.opacity = 1;
-      const video = document.getElementById("videoPlayer");
-      video.src = "b.mp4";
-      video.load();
-      video?.play();
-    }, 1000); // Delay in milliseconds
+      videoPlayer.src = videoManager[videoIndex].videoName;
+      videoPlayer.load();
+      videoPlayer.play();
+    }
   };
 
   return (
     <div className="videoContainer" id="videoContainer1">
-      <video
-        style={{ position: "relative" }}
-        id="videoPlayer"
-        width="100%"
-        height="100%"
-        controls
-      >
-        <source src="a.mp4" type="video/mp4" />
+      <video id="videoPlayer" width="100%" height="100%" controls>
+        <source src={currentVideo} type="video/mp4" />
       </video>
-      <button className="centered-button opacity0" onClick={toggleVideo}>
-        Toggle Video
-      </button>
-      <button className="centered-button2 opacity0" onClick={toggleVideo}>
-        Toggle Video2
-      </button>
-      <button className="centered-button3 opacity0" onClick={toggleVideo}>
-        Toggle Video3
-      </button>
+      {videoManager[currentIndex].buttons.map((button, idx) => (
+        <button
+          key={idx}
+          className={`${button.position}-button opacity0`}
+          onClick={() => {
+            if (button.nextVideo) {
+              changeVideo(button.nextVideo);
+            }
+          }}
+        >
+          {button.nextVideo ? (
+            <img src={button.buttonImage} alt="Button Icon" />
+          ) : (
+            <>לקריאת החלום</>
+          )}
+        </button>
+      ))}
     </div>
   );
 }
